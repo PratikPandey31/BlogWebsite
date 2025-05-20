f038392cbe432a2f0fdc24c64c7aabe5815d9784
Pratik <pratikofficial200@gmail.com>
2025-05-20 06:15:26 +0530

    Update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,19 +2,21 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig(({ mode }: { mode: string }) => ({
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    ...(mode === 'development' ? [] : []), 
+    ...(mode === "development" ? [] : []),
   ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  build: {
+    outDir: "dist", // Ensures build output goes to 'dist'
   },
 }));
